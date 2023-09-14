7d319cd2c1f380046b14fb977dc431774c76254f
A032003 <gholaseayushi@gmail.com>
2023-09-14 21:11:39 +0530

    my commit

---
+++ even no.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	
+	int num,i,a;
+	printf("ENTER digit:\n");
+scanf("%d",&num);
+
+printf("how many times:\n");
+scanf("%d",&a);
+for(i=2;i<=a;i+2)
+{
+	
+	printf("%d\n",i);
+}
+
+return 0;
+}
